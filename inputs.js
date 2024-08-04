bfd29ebd0ae75ba2cc9df6b538bda76b315a7d4a
MrCalhau <hugoaston02@gmail.com>
2024-08-04 13:40:38 +0100

    Update inputs.js

--- inputs.js
+++ inputs.js
@@ -1,17 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const testsquare = document.querySelector('.testsquare');
-    let toggle = true;
 
-    testsquare.addEventListener('click', function() {
-        if (toggle) {
-            testsquare.classList.remove('aniback');
-            testsquare.classList.add('anigo');
-            console.log("go");
-        } else {
-            testsquare.classList.remove('anigo');
-            testsquare.classList.add('aniback');
-            console.log("back");
-        }
-        toggle = !toggle;
-    });
-});
